5ec47f32ccad6d6cf36218df01470443a8f7634e
adrianadelnero <adriana.delnero@gmail.com>
2015-11-11 23:38:44 -0200

    Inclusao de campos de controle de data na entidade login

--- src/main/java/br/com/autentication/integration/dao/LoginDao.java
+++ src/main/java/br/com/autentication/integration/dao/LoginDao.java
@@ -1,5 +1,17 @@
 package br.com.autentication.integration.dao;
 
-public class LoginDao {
+import org.springframework.stereotype.Repository;
+import br.com.autentication.business.entity.Login;
+
+/**
+*
+* @author aromano
+*/
+@Repository(value = "loginRepository")
+public class LoginDao extends CrudMasterDetailJpaDAO<Login> {
+
+	public LoginDao() {
+		super(Login.class);
+	}
 
 }
